797a81724145ed3675d9cf9df976c9dc8f3f2f24
Max <kirienko.max@gmail.com>
2019-04-22 23:21:10 +0200

    xz

--- cmake/CopperConfigTargets.cmake
+++ cmake/CopperConfigTargets.cmake
@@ -1,12 +1,17 @@
 function(copper_config_framework LIBNAME)
   message("Configuring ${LIBNAME} framework")
 
-  set_target_properties(${LIBNAME} PROPERTIES FRAMEWORK TRUE)
+  set_target_properties(${LIBNAME} PROPERTIES 
+    FRAMEWORK TRUE
+    FRAMEWORK_VERSION "${PROJECT_VERSION}" 
+    MACOSX_FRAMEWORK_IDENTIFIER "com.redsoft.copperFramework"
+  )
 
   # Set the INSTALL_PATH so that frameworks can be installed in the application package
-  set_target_properties(${LIBNAME}
-     PROPERTIES BUILD_WITH_INSTALL_RPATH 1
-     INSTALL_NAME_DIR "@executable_path/../../../Frameworks"
+  set_target_properties( ${LIBNAME} PROPERTIES 
+    BUILD_WITH_INSTALL_RPATH 1
+    INSTALL_NAME_DIR "${CMAKE_INSTALL_PREFIX}/Frameworks"
+    #INSTALL_NAME_DIR "@executable_path/../../../Frameworks"
   )
 
   set_target_properties(${LIBNAME} PROPERTIES PUBLIC_HEADER "${HEADER_FILES};${PLATFORM_HEADERS};" )
